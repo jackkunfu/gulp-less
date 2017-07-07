710424e5dc02055a8dcd64edde6008f7414f6649
shanguoqiang <guoqiang.shan@healthbok.com>
2017-07-07 17:13:25 +0800

    gengxin

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function(){
+	gulp.src('./less/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('./css/'))
+})
+
+gulp.task('default', ['less'], function(){
+	gulp.watch('./less/*.less', ['less'])
+})
